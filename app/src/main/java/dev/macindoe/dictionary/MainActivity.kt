27f6500dafbb66e8202b9a7a2eed440e471f70e2
James Macindoe <james.macindoe@gmail.com>
2020-03-30 11:37:37 +1100

    Add bottom navigation bar

--- app/src/main/java/dev/macindoe/dictionary/MainActivity.kt
+++ app/src/main/java/dev/macindoe/dictionary/MainActivity.kt
@@ -1,12 +1,23 @@
 package dev.macindoe.dictionary
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        NavigationUI.setupWithNavController(bottom_nav, navHostFragment.navController)
     }
 }
---
+++ app/src/main/java/dev/macindoe/dictionary/features/favorites/FavoritesFragment.kt
@@ -0,0 +1,17 @@
+package dev.macindoe.dictionary.features.favorites
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.macindoe.dictionary.R
+
+class FavoritesFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_favorites, container, false)
+    }
+}
