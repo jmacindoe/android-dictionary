2c42344296a2f3466a7818a8d72c0c27f8fee247
James Macindoe <james.macindoe@gmail.com>
2020-03-30 15:05:56 +1100

    Add Gson and JSON initial data file

--- app/src/main/java/dev/macindoe/dictionary/data/InitialDataImportWorker.kt
+++ app/src/main/java/dev/macindoe/dictionary/data/InitialDataImportWorker.kt
@@ -3,8 +3,14 @@ package dev.macindoe.dictionary.data
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.google.gson.stream.JsonReader
 import org.koin.core.KoinComponent
 import org.koin.core.inject
+import java.lang.Exception
+
+private const val DATA_FILENAME = "words.json"
 
 class InitialDataImportWorker(
     context: Context,
@@ -14,13 +20,22 @@ class InitialDataImportWorker(
     val db: AppDatabase by inject()
 
     override suspend fun doWork(): Result {
-        db.wordDao()
-            .insertAll(
-                listOf(
-                    Word("1", "zh1 new", "pinyin1", "en1", false),
-                    Word("2", "zh2", "pinyin2", "en2", true)
-                )
-            )
+        try {
+            applicationContext.assets.open(DATA_FILENAME).use { inputStream ->
+                JsonReader(inputStream.reader()).use { jsonReader ->
+                    val type = object : TypeToken<List<Word>>() {}.type
+                    val words: List<Word> = Gson().fromJson(jsonReader, type)
+
+                    db.wordDao().insertAll(words)
+
+                    Result.success()
+                }
+            }
+        } catch (error: Exception) {
+            // TODO: If this was a production app, we should report this error
+            Result.failure()
+        }
+
         return Result.success()
     }
 }
